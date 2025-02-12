f57a830b7bf0e7877be666b16e9f1743e55f28c7
Vitorhugofsousa <vitorhugofs2018@gmail.com>
2025-02-12 11:41:39 -0300

    leds com pwm em funcionamento

--- ADC.c
+++ ADC.c
@@ -3,73 +3,97 @@
 #include "hardware/adc.h"
 #include "hardware/gpio.h"
 #include "hardware/i2c.h"
-#include "hardware/uart.h"
 #include "hardware/clocks.h"
-#include "hardware/pwm.h"
+#include "hardware/pwm.h"     
 
-#define LED_PIN_GREEN 11
-#define LED_PIN_BLUE 12
-#define LED_PIN_RED 13
+
 #define BOTAO_A 5 //pino saida botao a
 #define BOTAO_B 6 //pino saida botao b
-#define I2C_PORT i2c1 //porta I2C
+#define LED_PIN_GREEN 11 //pino saida led verde
+#define LED_PIN_BLUE 12 //pino saida led azul
+#define LED_PIN_RED 13  //pino saida led vermelho
 #define I2C_SDA 14 //pino SDA
 #define I2C_SCL 15 //pino SCL
+#define SW_PIN 22 //pino do botão SW JOYSTICK
+#define VRX_PIN 26   //pino do eixo X do joystick
+#define VRY_PIN 27   //pino do eixo Y do joystick
+#define I2C_PORT i2c1 //porta I2C
 #define display_address 0x3C //endereço do display
 
-const int VRX = 26;
-const int VRY = 27;
-const int ADC_CHANNEL_0 = 0;
-const int ADC_CHANNEL_1 = 1;
-const int SW = 22;
-const float DIVISOR = 16.0;
-const uint16_t PERIODO = 4096;
-uint16_t red_led_level, green_led_level, blue_led_level;
-uint slice_red, slice_green, slice_blue;
+uint pwm_init_gpio(uint gpio, uint wrap) {
+    gpio_set_function(gpio, GPIO_FUNC_PWM);
 
+    uint slice_num = pwm_gpio_to_slice_num(gpio);
+    pwm_set_wrap(slice_num, wrap);
 
-void setup_pwm_led(uint led, uint *slice, uint level){
-    gpio_set_function(led, GPIO_FUNC_PWM);
-    *slice = pwm_gpio_to_slice_num(led);
-    pwm_set_clkdiv(*slice, DIVISOR);
-    pwm_set_wrap(*slice, PERIODO);
-    pwm_set_gpio_level(led, level);
-    pwm_set_enabled(*slice, true);
+    pwm_set_enabled(slice_num, true);
+    return slice_num;
 }
-
-void joystick_read_axis(uint16_t *vrx_value, uint16_t *vry_value){
-    adc_select_input(ADC_CHANNEL_0);
-    sleep_us(2);
-    *vrx_value = adc_read();
-    adc_select_input(ADC_CHANNEL_1);
-    sleep_us(2);
-    *vry_value = adc_read();
-}
-
 
 int main(){
     stdio_init_all();
     adc_init();
-    adc_gpio_init(VRX);
-    adc_gpio_init(VRY);
-    gpio_init(SW);
-    gpio_set_dir(SW, GPIO_IN);
-    gpio_pull_up(SW);
-    setup_pwm_led(LED_PIN_RED, &slice_red, 0);
-    setup_pwm_led(LED_PIN_GREEN, &slice_green, 0);
-    setup_pwm_led(LED_PIN_BLUE, &slice_blue, 0);
-
-
-
-    uint16_t vrx_value, vry_value;
-    printf("Iniciando...\n");
+    adc_gpio_init(VRX_PIN); 
+    adc_gpio_init(VRY_PIN); 
+    gpio_init(SW_PIN);
+    gpio_set_dir(SW_PIN, GPIO_IN);
+    gpio_pull_up(SW_PIN); 
+    gpio_init(LED_PIN_GREEN);
+    gpio_set_dir(LED_PIN_GREEN, GPIO_OUT);
+    gpio_put(LED_PIN_GREEN, false); 
+    gpio_init(LED_PIN_RED);
+    gpio_set_dir(LED_PIN_RED, GPIO_OUT);
+    gpio_put(LED_PIN_RED, false); 
+    gpio_init(LED_PIN_BLUE);
+    gpio_set_dir(LED_PIN_BLUE, GPIO_OUT);
+    gpio_put(LED_PIN_BLUE, false);
+    
+    uint pwm_wrap = 4096;  
+    uint red_pwm_slice = pwm_init_gpio(LED_PIN_RED, pwm_wrap);  
+    uint blue_pwm_slice = pwm_init_gpio(LED_PIN_BLUE, pwm_wrap);  
+    uint32_t last_print_time = 0; 
+    
 
     while (true) {
-    joystick_read_axis(&vrx_value, &vry_value);
-    pwm_set_gpio_level(LED_PIN_RED, vrx_value);   
-    pwm_set_gpio_level(LED_PIN_GREEN, vry_value);
-    pwm_set_gpio_level(LED_PIN_BLUE, gpio_get(SW));
+        adc_select_input(0); 
+        uint16_t vrx_value = adc_read(); 
+
+        adc_select_input(1); 
+        uint16_t vry_value = adc_read(); 
+
+        int red_level = 0;
+        bool sw_value = gpio_get(SW_PIN) == 0; 
         
-    sleep_ms(100);
+        if (vrx_value > 2400) {
+            red_level = vrx_value - 2400; 
+        } else if (vrx_value < 1700){
+            red_level = 1700 - vrx_value;
+        }
+        
+        int blue_level = 0;
+        if (vry_value > 2400) {
+            blue_level = vry_value - 2400; 
+        } else if (vry_value < 1700){
+            blue_level = 1700 - vry_value;
+        }
+        
+        
+        if (sw_value) { 
+            gpio_put(LED_PIN_GREEN, true); 
+        } else {
+            gpio_put(LED_PIN_GREEN, false);
+        }
+
+        pwm_set_gpio_level(LED_PIN_RED, red_level);
+        pwm_set_gpio_level(LED_PIN_BLUE, blue_level);
+
+
+        uint32_t current_time = to_ms_since_boot(get_absolute_time());
+        if (current_time - last_print_time > 1000) {
+            printf("VRX: %u, VRY: %u, SW: %d\n", vrx_value, vry_value, sw_value);
+            last_print_time = current_time;
+        }
+        sleep_ms(100);
     }
+    return 0;
 }
